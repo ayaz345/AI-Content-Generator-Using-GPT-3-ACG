76532d9f41948412d3b6c0f93a384c636a2b30fb
Sourcery AI <>
2023-06-23 09:55:37 +0000

    'Refactored by Sourcery'

--- aicontent.py
+++ aicontent.py
@@ -7,13 +7,14 @@ openai.api_key = config.OPENAI_API_KEY
 def aicontent(query):
     response = openai.Completion.create(
         engine="davinci-instruct-beta",
-        prompt="Generate a detailed product description for:{}".format(query),
+        prompt=f"Generate a detailed product description for:{query}",
         temperature=0.8,
         max_tokens=256,
         top_p=1,
         frequency_penalty=0,
-        presence_penalty=0)
-    
+        presence_penalty=0,
+    )
+
     if 'choices' in response:
         if len(response['choices']) >0:
              answer = response['choices'][0]['text']
--- app.py
+++ app.py
@@ -20,93 +20,93 @@ def index():
 @app.route('/product-description', methods=["GET", "POST"])
 def productDescription(): #product description
 
-    if request.method == 'POST':
-        query = request.form['productDescription']
-        openAIAnswer =   aicontent.aicontent(query)
-        prompt = 'AI Suggestions for {} are:'.format(query)
+  if request.method == 'POST':
+    query = request.form['productDescription']
+    openAIAnswer =   aicontent.aicontent(query)
+    prompt = f'AI Suggestions for {query} are:'
 
-    return render_template('product-description.html', **locals())
+  return render_template('product-description.html', **locals())
 
 
 
 @app.route('/job-description', methods=["GET", "POST"])
 def jobDescription(): #job description
 
-    if request.method == 'POST':
-        query = request.form['jobDescription']
-        openAIAnswer =   aicontent.aicontent(query)
-        prompt = 'AI Suggestions for {} are:'.format(query)
+  if request.method == 'POST':
+    query = request.form['jobDescription']
+    openAIAnswer =   aicontent.aicontent(query)
+    prompt = f'AI Suggestions for {query} are:'
 
-    return render_template('job-description.html', **locals())
+  return render_template('job-description.html', **locals())
 
 
 
 @app.route('/tweet-ideas', methods=["GET", "POST"])
 def tweetIdeas(): #tweet ideas
 
-    if request.method == 'POST':
-        query = request.form['tweetIdeas']
-        openAIAnswer =   aicontent.aicontent(query)
-        prompt = 'AI Suggestions for {} are:'.format(query)
+  if request.method == 'POST':
+    query = request.form['tweetIdeas']
+    openAIAnswer =   aicontent.aicontent(query)
+    prompt = f'AI Suggestions for {query} are:'
 
-    return render_template('tweet-ideas.html', **locals())
+  return render_template('tweet-ideas.html', **locals())
 
 
 
 @app.route('/cold-emails', methods=["GET", "POST"])
 def coldEmails(): #coldemails
 
-    if request.method == 'POST':
-        query = request.form['coldEmails']
-        openAIAnswer =   aicontent.aicontent(query)
-        prompt = 'AI Suggestions for {} are:'.format(query)
+  if request.method == 'POST':
+    query = request.form['coldEmails']
+    openAIAnswer =   aicontent.aicontent(query)
+    prompt = f'AI Suggestions for {query} are:'
 
-    return render_template('cold-emails.html', **locals())
+  return render_template('cold-emails.html', **locals())
 
 
 
 @app.route('/social-media', methods=["GET", "POST"])
 def socialMedia(): #socialmedia
 
-    if request.method == 'POST':
-        query = request.form['socialMedia']
-        openAIAnswer =   aicontent.aicontent(query)
-        prompt = 'AI Suggestions for {} are:'.format(query)
+  if request.method == 'POST':
+    query = request.form['socialMedia']
+    openAIAnswer =   aicontent.aicontent(query)
+    prompt = f'AI Suggestions for {query} are:'
 
-    return render_template('social-media.html', **locals())
+  return render_template('social-media.html', **locals())
 
 
 @app.route('/business-pitch', methods=["GET", "POST"])
 def businessPitch(): #Marketing 
 
-    if request.method == 'POST':
-        query = request.form['businessPitch']
-        openAIAnswer =   aicontent.aicontent(query)
-        prompt = 'AI Suggestions for {} are:'.format(query)
+  if request.method == 'POST':
+    query = request.form['businessPitch']
+    openAIAnswer =   aicontent.aicontent(query)
+    prompt = f'AI Suggestions for {query} are:'
 
-    return render_template('business-pitch.html', **locals())
+  return render_template('business-pitch.html', **locals())
 
 
 @app.route('/video-ideas', methods=["GET", "POST"])
 def videoIdeas(): #youtube
 
-    if request.method == 'POST':
-        query = request.form['videoIdeas']
-        openAIAnswer =   aicontent.aicontent(query)
-        prompt = 'AI Suggestions for {} are:'.format(query)
+  if request.method == 'POST':
+    query = request.form['videoIdeas']
+    openAIAnswer =   aicontent.aicontent(query)
+    prompt = f'AI Suggestions for {query} are:'
 
-    return render_template('video-ideas.html', **locals())
+  return render_template('video-ideas.html', **locals())
 
 
 @app.route('/video-description', methods=["GET", "POST"])
 def videoDescription(): #videodesc
 
-    if request.method == 'POST':
-        query = request.form['videoDescription']
-        openAIAnswer =   aicontent.aicontent(query)
-        prompt = 'AI Suggestions for {} are:'.format(query)
+  if request.method == 'POST':
+    query = request.form['videoDescription']
+    openAIAnswer =   aicontent.aicontent(query)
+    prompt = f'AI Suggestions for {query} are:'
 
-    return render_template('video-description.html', **locals())
+  return render_template('video-description.html', **locals())
 
 
 
